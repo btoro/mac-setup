c203f12592c3650a133fb99cda11491c3fbb2c08
Botros <btoro93@gmail.com>
2025-07-07 15:59:29 -0500

    Add setup script for macOS environment configuration

---
+++ setup.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+
+# Create a folder who contains downloaded things for the setup
+INSTALL_FOLDER=~/.macsetup
+mkdir -p $INSTALL_FOLDER
+MAC_SETUP_PROFILE=$INSTALL_FOLDER/macsetup_profile
+
+# install brew
+if ! hash brew
+then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew update
+else
+  printf "\e[93m%s\e[m\n" "You already have brew installed."
+fi
+
+# CURL / WGET
+brew install curl
+brew install wget
+
+{
+  # shellcheck disable=SC2016
+  echo 'export PATH="/usr/local/opt/curl/bin:$PATH"'
+  # shellcheck disable=SC2016
+  echo 'export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"'
+  # shellcheck disable=SC2016
+  echo 'export PATH="/usr/local/opt/sqlite/bin:$PATH"'
+}>>$MAC_SETUP_PROFILE
+
+# git
+brew install git                                                                                      # https://formulae.brew.sh/formula/git
+# Adding git aliases (https://github.com/thomaspoignant/gitalias)
+git clone https://github.com/thomaspoignant/gitalias.git $INSTALL_FOLDER/gitalias && echo -e "[include]\n    path = $INSTALL_FOLDER/gitalias/.gitalias\n$(cat ~/.gitconfig)" > ~/.gitconfig
+
+brew install github
+
+# ZSH
+brew install zsh zsh-completions                                                                      # Install zsh and zsh completions
+sudo chmod -R 755 /usr/local/share/zsh
+sudo chown -R root:staff /usr/local/share/zsh
+{
+  echo "if type brew &>/dev/null; then"
+  echo "  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH"
+  echo "  autoload -Uz compinit"
+  echo "  compinit"
+  echo "fi"
+} >>$MAC_SETUP_PROFILE
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"# Install oh-my-zsh on top of zsh to getting additional functionality
+# Terminal replacement https://www.iterm2.com
+brew install --cask iterm2
+# Pimp command line
+brew install lsd                                                                                      # replacement for ls
+{
+  echo "alias ls='lsd'"
+  echo "alias l='ls -l'"
+  echo "alias la='ls -a'"
+  echo "alias lla='ls -la'"
+  echo "alias lt='ls --tree'"
+} >>$MAC_SETUP_PROFILE
+
+brew install tree
+brew install ack
+brew install bash-completion
+brew install jq
+brew install htop
+brew install coreutils
+brew install watch
+
+brew install --cask raycast
+brew install --cask openvpn-connect    
+brew install --cask calibre
+brew install --cask stats
+brew install --cask lunar
+
+brew install z
+touch ~/.z
+echo '. /usr/local/etc/profile.d/z.sh' >> $MAC_SETUP_PROFILE
+
+brew install ctop
+
+# Browser
+brew install --cask google-chrome
+brew install --cask firefox
+
+# Music / Video
+brew install --cask spotify
+
+
+# Communication
+brew install --cask slack
+brew install --cask whatsapp
+
+
+# IDE
+brew install --cask cursor
+brew install --cask visual-studio-code
+
+# Language
+## Node / Javascript
+mkdir ~/.nvm
+brew install nvm                                                                                     # choose your version of npm
+nvm install node                                                                                     # "node" is an alias for the latest version
+brew install pnpm                                                                                    # Dependencies management for node
+
+
+
+## python
+echo "export PATH=\"/usr/local/opt/python/libexec/bin:\$PATH\"" >> $MAC_SETUP_PROFILE
+brew install python
+pip install --user pipenv
+pip install --upgrade setuptools
+pip install --upgrade pip
+# shellcheck disable=SC2016
+echo 'eval "$(pyenv init -)"' >> $MAC_SETUP_PROFILE
+
+curl -sSf https://rye.astral.sh/get | bash
+
+
+
+## terraform
+brew install tfenv
+brew install terraform
+terraform -v
+
+# Databases
+brew install --cask dbeaver-community # db viewer
+
+
+# SFTP
+brew install --cask cyberduck
+
+# Docker
+brew install --cask orbstack
+brew install bash-completion
+brew install docker-completion
+brew install docker-compose-completion
+brew install docker-machine-completion
+
+brew install --cask google-cloud-sdk # Official command line
+
+brew install kubectl
+brew install --cask headlamp
+
+# reload profile files.
+{
+  echo "source $MAC_SETUP_PROFILE # alias and things added by mac_setup script"
+}>>"$HOME/.zsh_profile"
+# shellcheck disable=SC1090
+source "$HOME/.zsh_profile"
+
+{
+  echo "source $MAC_SETUP_PROFILE # alias and things added by mac_setup script"
+}>>~/.bash_profile
+# shellcheck disable=SC1090
+source ~/.bash_profile
